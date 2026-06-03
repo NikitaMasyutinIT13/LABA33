dcf07fc7e2116569ebdd2fd4ed51176eb1c49c60
NikitaMasyutinIT13 <nikitamas58@gmail.com>
2026-06-03 16:08:38 +0500

    Update ShapeSum.java

--- ShapeSum.java
+++ ShapeSum.java
@@ -1,38 +1,37 @@
 package ru.masyutin.geometry;
 
-import java.util.Scanner;
-
-import ru.masyutin.geometry.*;
-import java.util.Scanner;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Класс ShapeSum содержит методы для работы с геометрическими фигурами
  * Предоставляет методы для вычислений и вывода информации о фигурах
  */
+//FIXME: п.7 методы в PascalCase, п.17 удалить неиспользуемые импорты 
+// import java.util.Scanner;
+// import ru.masyutin.geometry.*;
+// import java.util.Scanner;
+// import java.util.ArrayList;
+// import java.util.List;
+//FIXTO:
 public class ShapeSum {
 
-
-  //Вычисление общей площади коллекции фигур
-  public static double calculateTotalArea(List<Shape> shapes) {
-    double totalArea = 0.0;
-    for (Shape shape : shapes) {
-      totalArea += shape.getArea();
-    }
-    return totalArea;
-  }
-
-  //Вывод информации о всех фигурах в списке
-  public static void printShapesInfo(List<Shape> shapes) {
-    if (shapes.isEmpty()) {
-      System.out.println("Список фигур пуст");
-      return;
+    public static double CalculateTotalArea(List<Shape> shapes) {
+        double total_Area = 0.0;
+        for (Shape shape : shapes) {
+            total_Area += shape.GetArea();
+        }
+        return total_Area;
     }
 
-    System.out.println("\n=== ИНФОРМАЦИЯ О ФИГУРАХ ===");
-    for (int i = 0; i < shapes.size(); i++) {
-      System.out.println((i + 1) + ". " + shapes.get(i).getInfo());
+    public static void PrintShapesInfo(List<Shape> shapes) {
+        if (shapes.isEmpty()) {
+            System.out.println("Список фигур пуст");
+            return;
+        }
+
+        System.out.println("\n=== ИНФОРМАЦИЯ О ФИГУРАХ ===");
+        for (int i = 0; i < shapes.size(); i++) {
+            System.out.println((i + 1) + ". " + shapes.get(i).GetInfo());
+        }
     }
-  }
 }
